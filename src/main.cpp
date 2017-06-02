50a9ebfebb3972988e17da58aa9b421bf0a97050
Dmitriy8726 <Agch6411@gmail.com>
2017-06-03 03:56:43 +0700

    Data input

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,14 @@ using namespace std;
 
 int main ()
 {
-	cout << "Hello,world";
+	int a,b,c;
+	cout << "a*(x*x)+b*x+c=0" << endl;
+	cout << "a = ";
+	cin << a;
+	cout << "b = ";
+	cin << b;
+	cout << "c = ";
+	cin << c;
 	cin.get();
 	return 0;
 }
